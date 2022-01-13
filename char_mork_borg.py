960bedd1c9a79e8438ef184c6f66edf0de9187d1
refrshrs <nthorgan@live.co.uk>
2022-01-13 19:12:29 +0000

    new

--- char_mork_borg.py
+++ char_mork_borg.py
@@ -2,8 +2,10 @@ from typing import List
 from random import randint, choice, choices
 from chargen import roll, expand, yaml_importer
 
-monk_borg_data = yaml_importer('mork_borg_data/data.yaml')
-starting_items = monk_borg_data['starting_items']
+mork_borg_data = yaml_importer('mork_borg_data/data.yaml')
+starting_items = mork_borg_data['starting_items']
+scrolls = mork_borg_data['scrolls']
+optional_classes = mork_borg_data['optional classes']
 
 
 ability_score_bonuses = expand({
@@ -37,45 +39,50 @@ class Container:
     def __repr__(self):
         return f"{self.name} of {self.maximum_size} size.\nContains {', '.join(item for item in self.contents)}"
 
-class Poison:
-    def __init__(self, name: str, effect: str, doses: int):
-        self.name = name
-        self.effect = effect
-        self.maximum_doses = doses
-        self.current_doses = doses
-
-    def __repr__(self):
-        return f"{self.name}: {self.effect}.\nHas {self.maximum_doses} doses total and {self.current_doses} remaining."
-
-class Rope:
-    def __init__(self, name: str, length: int, measurment: str):
-        self.name = name
-        self.length = length
-        self.measurement = measurment
-
-    def __repr__(self):
-        return f"{self.name} of {self.length}{self.measurement}"
-
-
 backpack = Container('Backpack', 7)
 sack = Container('Sack', 10)
 small_wagon = Container('Small Wagon', 1_000)
 toolbox = Container('Toolbox', 4)
 toolbox.add_items(['10 Nails', 'Hammer', 'Small Saw', 'Tongs'])
 
-heavy_chain = Rope('Heavy Chain', 15, 'feet')
-rope = Rope('Rope', 30, 'feet')
-
-black_poison = Poison('Black Poison', f'Toughness DR14 or {roll("1d6")} damage & blind for one hour', 3)
-red_poison = Poison('Red Poison', f'Toughness DR12 or {roll("1d10")} damage', 3)
-
+def gen_random_scroll(scroll_type: str, current_scrolls: List = None):
+    if scroll_type.lower() not in ["unclean", "sacred"]:
+        scroll = None
+    else:
+        scroll = choice(scrolls[scroll_type])
+    if current_scrolls:
+        while scroll in current_scrolls:
+            scroll = choice(scrolls[scroll_type])
+    return scroll
 
 def gen_starting_items(abilities):
     prescence = abilities['Prescence']
-    item_set_1 = ["Nothing", "Nothing", backpack, sack, small_wagon]
-    item_set_2 = [rope, f"{prescence + 4} torches", f"Lanturn with oil for {prescence + 6} hours", f"Magnesium Strip", f""]
+    item_set_1 = [
+        "Nothing",
+        "Nothing",
+        backpack,
+        sack,
+        small_wagon,
+        "Donkey"
+    ]
+    item_set_2 = [
+        "Rope",
+        f"{prescence + 4} torches",
+        f"Lanturn with oil for {prescence + 6} hours",
+        f"Magnesium Strip",
+        f"{gen_random_scroll('unclean')}",
+        "Sharp needle",
+        f"Medicine chest - {prescence+4} uses (stops bleeding/infection and heals 1d6 health"
+        "Metal file and lockpicks",
+        "Bear trap (Presence DR14 to spot, 1d8 damage)",
+        "Bomb (sealed bottle, 1d10 damage)",
+        f"A bottle of red poison with {roll('1d4')} doses (Toughness DR12 or 1d10 damage)"
+        "Silver crucifix"
+    ]
+    return [choice(item_set_1), choice(item_set_2)]
 
-def gen_character(classless: bool = True):
+
+def gen_character(classless: bool = True, character_class = None):
     if classless:
         ability_rerolls = 2
     hp, party_initiative = 0, 0
@@ -91,9 +98,10 @@ def gen_character(classless: bool = True):
                     ability_rerolls -= 1
 
             abilities[ability] = ability_score_bonuses[sum(dice_rolls)]
-        else:
-            abilities[ability] = ability_score_bonuses[sum(roll('1d6') for _ in range(3))]
-
+        elif ability in (optional_classes[character_class][abilities].keys()):
+            abilities[ability] = ability_score_bonuses[sum(dice_rolls)]+ optional_classes[character_class][ability]
+    items = gen_starting_items(ability)
+    return f"Abilities: {abilities}\nItems: {items}"
 
 
 
