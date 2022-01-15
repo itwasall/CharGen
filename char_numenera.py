96a201dccd6e9ae022c4a9fae524e71e6b745a3f
refrshrs <nthorgan@live.co.uk>
2022-01-15 19:52:31 +0000

    restarted numenera

--- char_numenera.py
+++ char_numenera.py
@@ -1,42 +1,20 @@
-import yaml
-from random import randint, choice, choices
+from typing import List, Dict
+from chargen import roll, yaml_importer
 
-job = yaml.safe_load(open('numenera_data//classes.yaml'))
-cypher = yaml.safe_load(open('numenera_data//cyphers.yaml'))
+character_classes = yaml_importer('numenera_data\\classes_new.yaml')
 
-
-def stats(job):
-    stats = {'might':0, 'speed':0, 'int':0}
-    stat_weights = job['stat_pool_weights']
-    for stat_name, stat_value in job['stats'].items():
-        stats[stat_name] = stat_value
-    for i in range(job['stat_pool']):
-        stat_boost = choices(list(stats.keys()), stat_weights)
-        stats[stat_boost[0]] += 1
-    return stats
-
-
-def cypher_gen(job):
-    items = job['equipment']
-    pop_list = []
-    for it, item in enumerate(items):
-        if item == 'rnd_cypher':
-            pop_list.append(it)
-            cyphergen = choice(list(cypher.keys()))
-            items.append(cyphergen)
-    pop_list.reverse()
-    for it in pop_list:
-        items.pop(it)
-    return items
-
-def char_gen():
-    char_job = choice(job['name'])
-    print(char_job[char_job['name']].keys())
-    char_stats = stats(char_job)
-    char_job['equipment'] = cypher_gen(char_job)
-
-
-    print(char_stats)
-    print(char_job['equipment'])
-
-char_gen()
+character = {
+    'Name': str,
+    'Focus': str,
+    'Job/Class': str,
+    'Stat Pools': {
+        'Might': List, # [Total, Remaining, Edge]
+        'Speed': List, # [Total, Remaining, Edge]
+        'Intellect': List  # [Total, Remaining, Edge]
+    },
+    'Equipment': List,
+    'Weapon': List, # [Weapon Name, Weapon Damage]
+    'Armour': List, # [Armour Name, Armour Damage]
+    'Cyphers': Dict, # {Cypher Name: {Effect: "", Level: "", Wearable: [y/n, conditions], Usable: [y/n, conditions]}
+    'Oddities': List
+}
