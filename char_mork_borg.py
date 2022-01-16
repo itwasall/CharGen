0788ad2f3aca3f853b394c6698c83723a16d27a2
refrshrs <nthorgan@live.co.uk>
2022-01-16 22:29:42 +0000

    Added Character Options 1 + Character Options 2 stuff revelant to classes_new.yaml

--- char_mork_borg.py
+++ char_mork_borg.py
@@ -20,14 +20,15 @@ BAD_HABITS = False
 TROUBLING_TALES = False
 
 ability_scores = expand({
-        range(1, 5): -3,
-        range(5, 7): -2,
-        range(7, 9): -1,
-        range(9, 13): 0,
-        range(13, 15): 1,
-        range(15, 17): 2,
-        range(17, 20): 3
+    range(1, 5): -3,
+    range(5, 7): -2,
+    range(7, 9): -1,
+    range(9, 13): 0,
+    range(13, 15): 1,
+    range(15, 17): 2,
+    range(17, 20): 3
 })
+
 
 class Container:
     def __init__(self, name: str, size: int):
@@ -50,9 +51,9 @@ class Container:
             else:
                 self.add_item(item)
 
-
     def __repr__(self):
         return f"{self.name} of {self.maximum_size} size.\n    Contains: {', '.join(item for item in self.contents)}"
+
 
 class Scroll:
     def __init__(self, name: str, desc: str, scroll_type: str):
@@ -75,6 +76,7 @@ sack = Container('Sack', 10)
 small_wagon = Container('Small Wagon', 1_000)
 toolbox = Container('Toolbox', 4)
 toolbox.add_items(['10 Nails', 'Hammer', 'Small Saw', 'Tongs'])
+
 
 def gen_random_scroll(scroll_type: str):
     unclean_scrolls = []
@@ -99,20 +101,23 @@ def gen_random_scroll(scroll_type: str):
         scroll = choice(sacred_scrolls)
     return scroll
 
-def gen_starting_items(abilities, character_class = None):
-    prescence = abilities['Prescence']
+
+def gen_starting_items(abilities, character_class=None):
+    presence = abilities['Prescence']
     item_set_1 = [None, None, backpack, sack, small_wagon, "Donkey"]
     item_set_2 = [
-        "Rope", f"{prescence + 4} torches", f"Lanturn with oil for {prescence + 6} hours", f"Magnesium Strip",
+        "Rope", f"{presence + 4} torches", f"Lanturn with oil for {presence + 6} hours", f"Magnesium Strip",
         f"{gen_random_scroll('unclean')}", "Sharp needle",
-        f"Medicine chest - {prescence+4} uses (stops bleeding/infection and heals 1d6 health",
-        "Metal file and lockpicks", "Bear trap (Presence DR14 to spot, 1d8 damage)", "Bomb (sealed bottle, 1d10 damage)",
+        f"Medicine chest - {presence + 4} uses (stops bleeding/infection and heals 1d6 health",
+        "Metal file and lockpicks", "Bear trap (Presence DR14 to spot, 1d8 damage)",
+        "Bomb (sealed bottle, 1d10 damage)",
         f"A bottle of red poison with {roll('1d4')} doses (Toughness DR12 or 1d10 damage)", "Silver crucifix"]
     item_set_3 = [
         f"Life elixir of {roll('1d4')} doses (heals 1d6 HP and removes infection)",
         f"{gen_random_scroll('sacred')}", f"Small but vicious dog ({roll('1d6') + 2}HP, bite 1d4, only obeys you)",
         f"{roll('1d4')} monkeys that ignore but love you (1d4+2 HP, punch/bite 1d4)", "exquisite perfume worth 25s",
-        toolbox, "Heavy chain, 15 feet", "grappling hook", "shield (-1 HP damage or have the shield break to ignore one attack)",
+        toolbox, "Heavy chain, 15 feet", "grappling hook",
+        "shield (-1 HP damage or have the shield break to ignore one attack)",
         "crowbar (1d4 damage)", "lard (may function as 5 meals in a pinch)", "tent"]
     NOT_BAGGABLE = [item_set_2[6], item_set_3[2], item_set_3[3]]
     item_1 = choice(item_set_1)
@@ -142,11 +147,13 @@ def gen_starting_items(abilities, character_class = None):
         regular_items.append(gen_random_scroll('random'))
     if armour_item[0] == 'Nothing' and armour_item[1] == 'None':
         armour_item = "No armour"
-    [regular_items.append(i) for i in [item_1, item_2, item_3, 'waterskin', f'{roll("1d4")} days of food'] if not i == None]
+    [regular_items.append(i) for i in [item_1, item_2, item_3, 'waterskin', f'{roll("1d4")} days of food'] if
+     not None]
     combat_items = [weapon_item, armour_item]
     return regular_items, combat_items, silver, omens
 
-def check_flags(flag_list: List, rnd = False):
+
+def check_flags(flag_list: List, rnd=False):
     flavor_text_flag_names = ['Traits', 'Bodies', 'Habit', 'Tales']
     if rnd:
         for _ in range(1, randint(1, 4)):
@@ -165,8 +172,8 @@ def check_flags(flag_list: List, rnd = False):
         TROUBLING_TALES = True
 
 
-def gen_character(character_class = None, rand_class: bool = False, flag_list: List = []):
-    global TERRIBLE_TRAITS, BROKEN_BODIES, BAD_HABITS, TROUBLING_TALES
+def gen_character(character_class=None, rand_class: bool = False, flag_list: List = []):
+    global TERRIBLE_TRAITS, BROKEN_BODIES, BAD_HABITS, TROUBLING_TALES, ability_re_rolls
     check_flags(flag_list, rnd=True)
     # Character name
     character_name = choice(names)
@@ -180,7 +187,7 @@ def gen_character(character_class = None, rand_class: bool = False, flag_list: L
     print(character_class)
     # Ability score rolling - Characters without a class get to reroll
     if not character_class:
-        ability_rerolls = 2
+        ability_re_rolls = 2
     character_abilities = {'Agility': 0, 'Prescence': 0, 'Strength': 0, 'Toughness': 0}
 
     for ability in character_abilities:
@@ -188,12 +195,13 @@ def gen_character(character_class = None, rand_class: bool = False, flag_list: L
         # Characters with a class can get a flat +/- modifier to a stat
         ability_mod = 0
         if not character_class:
-            if ability_scores[sum(dice_rolls)] < 0 and ability_rerolls > 0:
+            if (ability_scores[sum(dice_rolls)] < 0) and (ability_re_rolls > 0):
                 dice_rolls.reverse()
                 dice_rolls.pop()
                 dice_rolls.append(roll('1d6'))
-                ability_rerolls -= 1
-        elif character_class not in ['Wretched Royalty'] and ability in list(optional_classes[character_class]['abilities'].keys()):
+                ability_re_rolls -= 1
+        elif character_class not in ['Wretched Royalty'] and ability in list(
+                optional_classes[character_class]['abilities'].keys()):
             ability_mod = optional_classes[character_class]['abilities'][ability]
         character_abilities[ability] = ability_scores[sum(dice_rolls)] + ability_mod
 
@@ -227,7 +235,8 @@ def gen_character(character_class = None, rand_class: bool = False, flag_list: L
             for trait in optional_classes[character_class]['traits'].keys():
                 character_flavor_text += f"\n{trait}: {optional_classes[character_class]['traits'][trait]}"
 
-    return f"Abilities: {character_abilities}\nSilver: {character_silver}  Items: {character_items}\nWeapons + Armour: {character_combat_items}\nOmens: {character_omens}\nCharacter: {character_flavor_text}"
+    return f"Abilities: {character_abilities}\nSilver: {character_silver}  Items: {character_items}\n" \
+           f"Weapons + Armour: {character_combat_items}\nOmens: {character_omens}\nCharacter: {character_flavor_text}"
 
 
 print(gen_character())
