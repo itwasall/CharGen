faa46bf124af8c7c643fb86295cc887a0bb99a56
refrshrs <nthorgan@live.co.uk>
2022-01-18 10:33:41 +0000

    foci done

--- char_numenera.py
+++ char_numenera.py
@@ -1,7 +1,10 @@
 from typing import List, Tuple
+from random import choice
 from chargen import roll, yaml_importer
 
-character_classes = yaml_importer('numenera_data/classes_new.yaml')
+chartypes = yaml_importer('numenera_data/classes_new.yaml')
+chardescs = yaml_importer('numenera_data/descriptor.yaml')
+charfoci = yaml_importer('numenera_data/foci.yaml')
 
 character = {
     'Name': str,
@@ -26,4 +29,34 @@ character = {
     }, 
     'Oddities': List[str], # [Oddity description]
 }
+
+def gen_character(
+    settype: str = None,
+    setdesc: str = None,
+    setfoci: str = None
+):
+    if settype is None or settype not in list(chartypes.keys()):
+        choice_type = choice(list(chartypes.keys()))
+        character_type = (choice_type, chartypes[choice_type])
+    else:
+        character_type = (settype, chartypes[settype])
+    if setdesc is None or setdesc not in list(chardescs.keys()):
+        choice_desc = choice(list(chardescs.keys()))
+        character_descriptor = (choice_desc, chardescs[choice_desc])
+    else:
+        character_descriptor = (setdesc, chartypes[setdesc])
+    if setfoci is None or setfoci not in list(charfoci.keys()):
+        choice_foci = choice(list(charfoci.keys()))
+        character_focus = (choice_foci, charfoci[choice_foci])
+    else:
+        character_focus = (setfoci, charfoci[setfoci])
+    return (character_type, character_descriptor, character_focus)
+
+character_type, character_descriptor, character_focus = gen_character(setfoci='Sees Beyond')
+character['Type'] = character_type[0]
+character['Descriptor'] = character_descriptor[0]
+character['Focus'] = character_focus[0]
+
+
+print(character)
 
