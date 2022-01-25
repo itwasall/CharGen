077a0ec164706a42069385ca1957e927eeec2cb4
refrshrs <nthorgan@live.co.uk>
2022-01-25 10:20:12 +0000

    numenera AGAIN

--- char_numenera.py
+++ char_numenera.py
@@ -9,29 +9,108 @@ charfoci = yaml_importer('numenera_data/foci.yaml')
 foci_key_exceptions = ['trained', 'cost', 'choice', 'pool_bonus', 'pool_bonus_cond', 'asset', 'language_add', 'train_choice', 'train_choice2', 'Servant', 'Philosophic Confusion', 'Zap', 'additional_points']
 pool_bonus_options = ['Might', 'Speed', 'Intellect', 'MightSpeed', 'MightIntellect', 'SpeedIntellect', 'MightEdge', 'SpeedEdge', 'IntellectEdge', 'Armour']
 
+class Cypher:
+    def __init__(
+        self,
+        cypher_name: str,
+        cypher_level: str,
+        cypher_effect: str,
+        wearable: tuple, # (bool, optional str)
+        useable: tuple # (bool, optional str)
+    ):
+        self.cypher_name = cypher_name.capitalize()
+        self.cypher_level = roll(cypher_level)
+        self.cypher_effect = cypher_effect
+        self.wearable = wearable
+        self.useable = useable
+
+    def __repr__(self): return self.name
+
+class Stat:
+    def __init__(
+        self,
+        maximum: int,
+        current: int = None
+    ):
+        self.maximum = maximum
+        if current is None:
+            self.current = maximum
+        else:
+            self.current = current
+
+    def __add__(self, x:int):
+        new_current = self.current + x
+        new_maximum = self.maximum + x
+        return Stat(new_maximum, new_current)
+    def __iadd__(self, x:int):
+        return self.__add__(x)
+
+    def __repr__(self):
+        return f"{self.maximum}/{self.current}"
+
+class StatPool:
+    def __init__(
+        self,
+        might: Stat,
+        speed: Stat,
+        intellect: Stat,
+    ):
+        self.Might = might
+        self.Speed = speed
+        self.Intellect = intellect
+
+    def __add__(self, value: int, stat: str):
+        stat = stat.capitalize()
+        if stat not in ['Might', 'Speed', 'Intellect']:
+            pass
+        elif stat == 'Might':
+            self.Might += value
+        elif stat == 'Speed':
+            self.Speed += value
+        elif stat == 'Intellect':
+            self.Intellect += value
+
+    def __repr__(self):
+        return f"Might: {self.Might} Speed: {self.Speed} Intellect: {self.Intellect}"
+
+class Edge:
+    def __init__(
+        self,
+        might_edge: int,
+        speed_edge: int,
+        intellect_edge: int
+    ):
+        self.might_edge = might_edge
+        self.speed_edge = speed_edge
+        self.intellect_edge = intellect_edge
+
+    def __add__(self, value: int, edge: str):
+        edge = edge.capitalize()
+        if edge not in ['Might', 'Speed', 'Intellect']:
+            pass
+        elif edge == 'Might':
+            self.might_edge += value
+        elif edge == 'Speed':
+            self.speed_edge += value
+        elif edge == 'Intellect':
+            self.intellect_edge += value
+
+    def __repr__(self):
+        return f"Might Edge: {self.might_edge} Speed Edge: {self.speed_edge} Intellect Edge: {self.intellect_edge}"
+
 character = {
     'Name': str,
     'Type': "",
     'Focus': tuple((str, str)),
     'Descriptor': tuple((str, str)),
-    'StatPools': {
-        'StatMight': List[int],  # [Total, Remaining, Edge]
-        'StatSpeed': List[int],  # [Total, Remaining, Edge]
-        'StatIntellect': List[int]  # [Total, Remaining, Edge]
-    },
+    'StatPools': StatPool,
+    'Edge': Edge,
     'Equipment': List,
     'Weapon': List,  # [Weapon Name, Weapon Damage]
     'Armour': List,  # [Armour Name, Armour Damage]
     'Abilities': List,
     'Inabilities': List,
-    'Cyphers': {
-        'CypherName': {
-            'CypherEffect': str,
-            'CypherLevel': tuple((str, int)),  # ("1d4", 2) -> 1d4 + 2
-            'CypherWearable': tuple((bool, str)),  # (y/n, conditions)
-            'CypherUsable': tuple((bool, str)),  # (y/n, conditions)
-        },
-    },
+    'Cyphers': List[Cypher],
     'Oddities': List[str],  # [Oddity description]
     'Skills': dict
 }
@@ -75,17 +154,15 @@ def gen_character(
 
     print(init_stat_pools)
 
-    total_might = init_stat_pools[0]['Might']
-    total_speed = init_stat_pools[0]['Speed']
-    total_intellect = init_stat_pools[0]['Intellect']
-    edge_might = init_stat_pools[1]['Might']
-    edge_speed = init_stat_pools[1]['Speed']
-    edge_intellect = init_stat_pools[1]['Intellect']
+    total_might = Stat(init_stat_pools[0]['Might'])
+    total_speed = Stat(init_stat_pools[0]['Speed'])
+    total_intellect = Stat(init_stat_pools[0]['Intellect'])
+    character_edge = Edge(init_stat_pools[1]['Might'], init_stat_pools[1]['Speed'], init_stat_pools[1]['Intellect'])
 
-    stat_pool_list = [total_might, total_speed, total_intellect, edge_might, edge_speed, edge_intellect]
+    # characrer_stats = [total_might, total_speed, total_intellect, edge_might, edge_speed, edge_intellect]
 
-    for it, pool in enumerate(list(character['StatPools'].keys())):
-        character['StatPools'][pool] = stat_pool_list[it]
+    character['StatPools'] = StatPool(total_might, total_speed, total_intellect)
+    character['Edge']
 
     return character
 
@@ -94,3 +171,8 @@ def gen_character(
 x = gen_character(verbose=True)
 for k in list(x.keys()):
     print(f"{k}: {x[k]}")
+
+character['StatPools'].__add__(2, 'Might')
+
+print(character['StatPools'])
+
