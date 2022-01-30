dd63b95db7c82507c386364e485959f954a65f47
refrshrs <nthorgan@live.co.uk>
2022-01-30 09:56:26 +0000

    exalted charms

---
+++ exalted_3e_data/yaml_test.py
@@ -0,0 +1,21 @@
+import yaml
+from random import choice
+
+data = yaml.safe_load(open('exalted_3e_data/charms.yaml', 'rt'))
+
+def choose_charm():
+    charm_key = choice(list(data.keys()))
+    charm = {charm_key: data[charm_key]}
+    return charm
+
+def print_charm_names():
+    outfile = open('charm_names.txt', 'wt')
+    for charm in list(data.keys()):
+        charm_og = charm
+        charm_modified = "_".join(charm.split(' '))
+        charm_modified = "".join(charm_modified.split('’'))
+        outfile.write(f"{charm_modified} {charm_og}\n")
+    outfile.close
+
+print(choose_charm())
+print_charm_names()
