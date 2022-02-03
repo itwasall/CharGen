aeb49f0a0c6387190745ba89691d7a0675c01451
refrshrs <nthorgan@live.co.uk>
2022-02-03 15:59:36 +0000

    mini-exalted

--- exalted_3e_data/mini_exalted/mini_exalted.py
+++ exalted_3e_data/mini_exalted/mini_exalted.py
@@ -1,10 +1,26 @@
 import sys
-import os
-import sys
-sys.path.append(f"F:\\CharGen\\exalted_3e_data")
-print(sys.path)
+from random import choice
+sys.path.append("F:\\CharGen")
 
-if __name__ == "__main__":
-    from exalted_3e_data import yaml_test
+from exalted_3e_data import data
+from exalted_3e_data.data import LIST_ABILITIES, LIST_ATTRIBUTE, LIST_CASTE
 
 
+class Character:
+    def __init__(self):
+        self.caste = None
+        self.abilities = {}
+        self.attributes = [k for k in LIST_ATTRIBUTE]
+
+def select_caste(character: Character):
+    caste_choice = choice(LIST_CASTE)
+    for ability in caste_choice.abilities:
+        if ability == "Brawl/Martial Arts":
+            ability = choice(["Brawl", "Martial Arts"])
+        character.abilities[ability] = [ab for ab in LIST_ABILITIES if ab.name == ability][0]
+        character.abilities[ability].is_caste = True
+
+jerry = Character()
+select_caste(jerry)
+print(jerry.abilities)
+print(jerry.attributes)
