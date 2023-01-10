bf72b3bceaa0500038eb1116168b16e81937ea53
refrshrs <nthorgan@live.co.uk>
2023-01-10 19:29:15 +0000

    added attributes

--- char_shadow_of_the_demon_lord.py
+++ char_shadow_of_the_demon_lord.py
@@ -35,6 +35,51 @@ class InterestingItem(ABC):
         super().__init__(name, **kwargs)
         InterestingItem.items.append(self)
 
+class Path(ABC):
+    items = []
+    def __init__(self, name, _type, **kwargs):
+        super().__init__(name, **kwargs)
+        self._type = _type
+        Path.items.append(self)
+
+class AttributeScore:
+    def __init__(self, name, value=0, **kwargs):
+        self.name = name
+        self.value = value
+
+    def __add__(self, value):
+        self.value = self.value + value
+        return AttributeScore(self.name, self.value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+    def __repr__(self):
+        return str(self.value)
+
+class AttributeBlock:
+    def __init__(self, strength: AttributeScore, agility: AttributeScore, intellect: AttributeScore, will: AttributeScore):
+        self.strength = strength
+        self.agility = agility
+        self.intellect = intellect
+        self.will = will
+
+    def __repr__(self):
+        return self.__format__()
+
+    def __format__(self):
+        self.return_string = f"Strength: {self.strength}\nAgility: {self.agility}\nIntellect: {self.intellect}\nWill: {self.will}"
+        return self.return_string
+    
+
+STRENGTH = AttributeScore('Strength')
+AGILITY = AttributeScore('Agility')
+INTELLECT = AttributeScore('Intellect')
+WILL = AttributeScore('Will')
+
+STAT_BLOCK = AttributeBlock(STRENGTH, AGILITY, INTELLECT, WILL)
+print(STAT_BLOCK)
+
 """
     ANCESTORIES
 """
@@ -52,7 +97,7 @@ ANCESTORIES = [HUMAN, CHANGELING, CLOCKWORK, DWARF, GOBLIN, ORC]
 # COMMON
 ANIMAL_TRAINER = Profession("Animal_Trainer", _type='Common')
 APOTHECARY = Profession("Apothecary", _type='Common')
-HEALER = Profession("Healer", _type='Common')
+HEALER_P = Profession("Healer", _type='Common')
 ARTISAN = Profession("Artisan", _type='Common', _subtype=['Baker', 'Blacksmith', 'Bookbinder', 'Brewer', 'Carpenter', 'Chandler', 'Cobbler', 'Dyer', 'Glassblower', 'Jeweler', 'Leatherworker', 'Mason', 'Potter', 'Printer', 'Tailor'])
 ARTIST = Profession("Artist", _type='Common', _subtype=['Painter', 'Poet', 'Sculptor', 'Writer'])
 BOATMAN = Profession("Boatman", _type='Common')
@@ -107,7 +152,7 @@ CULTIST = Profession("Cultist", _type='Criminal')
 FENCE = Profession("Fence", _type='Criminal')
 FORGER = Profession("Forger", _type='Criminal')
 GAMBLER = Profession("Gambler", _type='Criminal')
-GRAVE_ROBBER = Profession("Grave_Robber", _type='Criminal')
+GRAVE_ROBBER = Profession("Grave Robber", _type='Criminal')
 INFORMANT = Profession("Informant", _type='Criminal')
 MURDERER = Profession("Murderer", _type='Criminal')
 PICKPOCKET = Profession("Pickpocket", _type='Criminal')
@@ -127,9 +172,9 @@ JAILER = Profession("Jailer", _type='Martial')
 OFFICER = Profession("Officer", _type='Martial')
 MARINE = Profession("Marine", _type='Martial')
 MERCENARTY = Profession("Mercenarty", _type='Martial')
-MILITIA_MEMBER = Profession("Militia_Member", _type='Martial')
+MILITIA_MEMBER = Profession("Militia Member", _type='Martial')
 PARTOLLER = Profession("Partoller", _type='Martial')
-PEASANT_CONSCRIPT = Profession("Peasant_Conscript", _type='Martial')
+PEASANT_CONSCRIPT = Profession("Peasant Conscript", _type='Martial')
 SLAVE = Profession("Slave", _type='Martial')
 SOLIDER = Profession("Solider", _type='Martial')
 SQUIRE = Profession("Squire", _type='Martial')
@@ -137,7 +182,7 @@ TORTUER = Profession("Tortuer", _type='Martial')
 # WILDERNESS
 BANDIT = Profession("Bandit", _type='Wilderness')
 BRIGAND = Profession("Brigand", _type='Wilderness')
-HIGHWAY_ROBBER = Profession("Highway_Robber", _type='Wilderness')
+HIGHWAY_ROBBER = Profession("Highway Robber", _type='Wilderness')
 BARBARIAN = Profession("Barbarian", _type='Wilderness')
 EXILE = Profession("Exile", _type='Wilderness')
 GATHERER = Profession("Gatherer", _type='Wilderness')
@@ -161,13 +206,20 @@ DEVOTEE = Profession("Devotee", _type='Religious')
 EVANGELIST = Profession("Evangelist", _type='Religious')
 FLAGELLANT = Profession("Flagellant", _type='Religious')
 HERETIC = Profession("Heretic", _type='Religious')
-INITIATE_OF_THE_OLD_FAITH = Profession("Initiate_of_the_Old_Faith", _type='Religious')
+INITIATE_OF_THE_OLD_FAITH = Profession("Initiate of the Old Faith", _type='Religious')
 MINISTER = Profession("Minister", _type='Religious')
-ACOLYUTE_OF_THE_NEW_GOD = Profession("Acolyute_of_the_New_God", _type='Religious')
-INQUISITORS_HENCHMEN = Profession("Inquisitors_Henchmen", _type='Religious')
+ACOLYUTE_OF_THE_NEW_GOD = Profession("Acolyute of the New God", _type='Religious')
+INQUISITORS_HENCHMEN = Profession("Inquisitors Henchmen", _type='Religious')
 PILGRIM = Profession("Pilgrim", _type='Religious')
-STREET_PREACHER = Profession("Street_Preacher", _type='Religious')
-TEMPLE_WARD = Profession("Temple_Ward", _type='Religious')
+STREET_PREACHER = Profession("Street Preacher", _type='Religious')
+TEMPLE_WARD = Profession("Temple Ward", _type='Religious')
+
+PROFESSION_TYPES = []
+for _type in [i._type for i in Profession.items]:
+    if _type not in PROFESSION_TYPES:
+        PROFESSION_TYPES.append(_type)
+    else: 
+        continue
 
 """
     INTERESTING ITEMS
@@ -176,31 +228,31 @@ TEMPLE_WARD = Profession("Temple_Ward", _type='Religious')
 TABLE1_ITEM1 = InterestingItem("A tiny metal box with no opening that makes a faint ticking noise.")
 TABLE1_ITEM2 = InterestingItem("A skull made from clear crystal.")
 TABLE1_ITEM3 = InterestingItem("A glass ball filled with water in which swims a tiny living goldfish.")
-TABLE1_ITEM4 = InterestingItem("A curious odor, a pungent stench, or a skin condition that never quite heals.")
+TABLE1_ITEM4 = InterestingItem(f"{random.choice(['A curious odor', 'a pungent stench', 'a skin condition that never quite heals'])}.")
 TABLE1_ITEM5 = InterestingItem("A bottle filled with a maiden’s tears.")
 TABLE1_ITEM6 = InterestingItem("A flower that never withers.")
-TABLE1_ITEM7 = InterestingItem("A small magnet or silver mirror.")
-TABLE1_ITEM8 = InterestingItem("An invitation to a party or a masquerade mask.")
+TABLE1_ITEM7 = InterestingItem(f"{random.choice(['A small magnet', 'a silver mirror'])}.")
+TABLE1_ITEM8 = InterestingItem(f"An invitation to a {random.choice(['party', 'masquerade mask'])}.")
 TABLE1_ITEM9 = InterestingItem("A monogrammed handkerchief that always stays clean.")
 TABLE1_ITEM10 = InterestingItem("A folding knife that always stays sharp.")
 TABLE1_ITEM11 = InterestingItem("A pair of dancing shoes.")
 TABLE1_ITEM12 = InterestingItem("A tiny inert mechanical spider.")
 TABLE1_ITEM13 = InterestingItem("A shrunken head.")
-TABLE1_ITEM14 = InterestingItem("A glass eye or a bezoar.")
-TABLE1_ITEM15 = InterestingItem("A book written in an unknown language or a book containing things you never wanted to know.")
+TABLE1_ITEM14 = InterestingItem(f"{random.choice(['A glass eye', 'a bezoar'])}.")
+TABLE1_ITEM15 = InterestingItem(f"A book {random.choice(['written in an unknown language', 'a book containing things you never wanted to know'])}.")
 TABLE1_ITEM16 = InterestingItem("A deck of fortune-teller’s cards.")
 TABLE1_ITEM17 = InterestingItem("A pair of loaded dice.")
 TABLE1_ITEM18 = InterestingItem("Six small cakes that can nourish the person who eats one until the next day at dawn.")
 TABLE1_ITEM19 = InterestingItem("A phylactery that holds a scrap of paper on which is written a single word.")
-TABLE1_ITEM20 = InterestingItem("A reputation for being a badass1 A tiny metal box with no opening that makes a faint ticking noise.")
+TABLE1_ITEM20 = InterestingItem("A reputation for being a badass")
 ITEM_TABLE_1 = [TABLE1_ITEM1, TABLE1_ITEM2, TABLE1_ITEM3, TABLE1_ITEM4, TABLE1_ITEM5, TABLE1_ITEM6, TABLE1_ITEM7, TABLE1_ITEM8, TABLE1_ITEM9, TABLE1_ITEM10, TABLE1_ITEM11, TABLE1_ITEM12, TABLE1_ITEM13, TABLE1_ITEM14, TABLE1_ITEM15, TABLE1_ITEM16, TABLE1_ITEM17, TABLE1_ITEM18, TABLE1_ITEM19, TABLE1_ITEM20]
 # TABLE 2
-TABLE2_ITEM1 = InterestingItem(f"A flute or set of panpipes, or other musical instrument.")
+TABLE2_ITEM1 = InterestingItem(f"{random.choice(['A flute', 'a set of panpipes', 'a musical instrument'])}.")
 TABLE2_ITEM2 = InterestingItem(f"A reliquary containing a small bone.")
 TABLE2_ITEM3 = InterestingItem(f"A tiny idol of a demon carved from green stone.")
-TABLE2_ITEM4 = InterestingItem(f"A token from an admirer or lover.")
-TABLE2_ITEM5 = InterestingItem(f"A pet mouse, squirrel, or rabbit.")
-TABLE2_ITEM6 = InterestingItem(f"A monocle or pair of heavy goggles.")
+TABLE2_ITEM4 = InterestingItem(f"A token from {random.choice(['an admirer', 'a lover'])}.")
+TABLE2_ITEM5 = InterestingItem(f"A pet {random.choice(['mouse', 'squirrel', 'rabbit'])}.")
+TABLE2_ITEM6 = InterestingItem(f"{random.choice(['A monocle', 'A pair of heavy goggles'])}.")
 TABLE2_ITEM7 = InterestingItem(f"A silver necklace with a medallion.")
 TABLE2_ITEM8 = InterestingItem(f"A snuffbox filled with snuff.")
 TABLE2_ITEM9 = InterestingItem(f"A gleaming dragon’s scale.")
@@ -208,24 +260,24 @@ TABLE2_ITEM10= InterestingItem(f"A fist-sized egg covered in blue spots.")
 TABLE2_ITEM11= InterestingItem(f"Unrequited love.")
 TABLE2_ITEM12= InterestingItem(f"A black iron cauldron filled with bones.")
 TABLE2_ITEM13= InterestingItem(f"A box of {dice('1d20')} iron nails.")
-TABLE2_ITEM14= InterestingItem(f"A vial of sweet perfume or a bottle of rotgut.")
+TABLE2_ITEM14= InterestingItem(f"{random.choice(['A vial of sweet perfume', 'A bottle of rotgut'])}.")
 TABLE2_ITEM15= InterestingItem(f"A feather made from bronze.")
-TABLE2_ITEM16= InterestingItem(f"An iron coin with a scratch on one side or a steel coin with a dragon’s head on either side.")
+TABLE2_ITEM16= InterestingItem(f"{random.choice(['An iron coin with a scratch on one side', 'a steel coin with a dragon’s head on either side'])}.")
 TABLE2_ITEM17= InterestingItem(f"A box containing {sum([dice('1d6'), 1])} brushes.")
 TABLE2_ITEM18= InterestingItem(f"A bloodstained doll.")
 TABLE2_ITEM19= InterestingItem(f"A silver engagement ring worth 1 ss.")
-TABLE2_ITEM20= InterestingItem(f"A brush, comb, or umbrella.")
+TABLE2_ITEM20= InterestingItem(f"{random.choice(['A brush', 'A comb', 'An umbrella'])}.")
 ITEM_TABLE_2 = [TABLE2_ITEM1, TABLE2_ITEM2, TABLE2_ITEM3, TABLE2_ITEM4, TABLE2_ITEM5, TABLE2_ITEM6, TABLE2_ITEM7, TABLE2_ITEM8, TABLE2_ITEM9, TABLE2_ITEM10, TABLE2_ITEM11, TABLE2_ITEM12, TABLE2_ITEM13, TABLE2_ITEM14, TABLE2_ITEM15, TABLE2_ITEM16, TABLE2_ITEM17, TABLE2_ITEM18, TABLE2_ITEM19, TABLE2_ITEM20]
 # TABLE 3
-TABLE3_ITEM1 = InterestingItem(f"A bar of soap or a towel.")
+TABLE3_ITEM1 = InterestingItem(f"{random.choice(['A bar of soap', 'A towel'])}.")
 TABLE3_ITEM2 = InterestingItem(f"One hundred feet of twine wrapped up in a ball.")
-TABLE3_ITEM3 = InterestingItem(f"A tiny portrait, a lock of hair, or some other favor from someone who loves you.")
+TABLE3_ITEM3 = InterestingItem(f"{random.choice(['A tiny portrait', 'a lock of hair', 'a favor'])} from someone who loves you.")
 TABLE3_ITEM4 = InterestingItem(f"A small keg of beer.")
-TABLE3_ITEM5 = InterestingItem(f"A brace of conies or pack filled with pots and pans.")
-TABLE3_ITEM6 = InterestingItem(f"An arrow or bolt with a silvered head.")
-TABLE3_ITEM7 = InterestingItem(f"Half a treasure map, a map of a foreign land, or a large, blue map covered with circles with weird bits of writing between them.")
+TABLE3_ITEM5 = InterestingItem(f"{random.choice(['A brace of conies', 'a pack'])} filled with pots and pans.")
+TABLE3_ITEM6 = InterestingItem(f"An arrow with a silvered head.")
+TABLE3_ITEM7 = InterestingItem(f"{random.choice(['Half a treasure map', 'a map of a foreign land', 'a large, blue map covered with circles with weird bits of writing between them'])}.")
 TABLE3_ITEM8 = InterestingItem(f"A weapon of the GM’s choice.")
-TABLE3_ITEM9 = InterestingItem(f"A light or heavy shield with an unusual heraldic device.")
+TABLE3_ITEM9 = InterestingItem(f"A {random.choice(['light', 'heavy'])} shield with an unusual heraldic device.")
 TABLE3_ITEM10= InterestingItem(f"A fancy set of clothes bearing a curious stain.")
 TABLE3_ITEM11= InterestingItem(f"A personal servant.")
 TABLE3_ITEM12= InterestingItem(f"A silver holy symbol or a fine religious icon.")
@@ -307,12 +359,98 @@ ITEM_TABLE_6 = [TABLE6_ITEM1, TABLE6_ITEM2, TABLE6_ITEM3, TABLE6_ITEM4, TABLE6_I
 # LIST
 ITEM_TABLES = [ITEM_TABLE_1, ITEM_TABLE_2, ITEM_TABLE_3, ITEM_TABLE_4, ITEM_TABLE_5, ITEM_TABLE_6]
 
-PROFESSION_TYPES = []
-for _type in [i._type for i in Profession.items]:
-    if _type not in PROFESSION_TYPES:
-        PROFESSION_TYPES.append(_type)
-    else: 
-        continue
+"""
+    PATHS
+"""
+# NOVICE
+MAGICIAN = Path("Magician", 'Novice')
+PRIEST = Path("Priest", 'Novice')
+ROGUE = Path("Rogue", 'Novice')
+WARRIOR = Path("Warrior", 'Novice')
+NOVICE_PATHS = [p for p in Path.items if p._type == 'Novice']
+# EXPERT
+ARTIFICER = Path("Artificer", 'Expert')
+ASSASSIN = Path("Assassin", 'Expert')
+BERSERKER = Path("Berserker", 'Expert')
+CLERIC = Path("Cleric", 'Expert')
+DRUID = Path("Druid", 'Expert')
+FIGHTER = Path("Fighter", 'Expert')
+ORACLE = Path("Oracle", 'Expert')
+PALADIN = Path("Paladin", 'Expert')
+RANGER = Path("Ranger", 'Expert')
+SCOUT = Path("Scout", 'Expert')
+SPELLBINDER = Path("Spellbinder", 'Expert')
+THIEF = Path("Thief", 'Expert')
+WARLOCK = Path("Warlock", 'Expert')
+WITCH = Path("Witch", 'Expert')
+WIZARD = Path("Wizard", 'Expert')
+EXPERT_PATHS = [p for p in Path.items if p._type == 'Expert']
+# MASTER
+ABJURER = Path("Abjurer", 'Master')
+ACROBAT = Path("Acrobat", 'Master')
+AEROMANCER = Path("Aeromancer", 'Master')
+APOCALYPTIST = Path("Apocalyptist", 'Master')
+ARCANIST = Path("Arcanist", 'Master')
+ASTROMANCER = Path("Astromancer", 'Master')
+AVENGER = Path("Avenger", 'Master')
+BARD = Path("Bard", 'Master')
+BEASTMASTER = Path("Beastmaster", 'Master')
+BLADE = Path("Blade", 'Master')
+BRUTE = Path("Brute", 'Master')
+CAVALIER = Path("Cavalier", 'Master')
+CHAMPION = Path("Champion", 'Master')
+CHAPLAIN = Path("Chaplain", 'Master')
+CHRONOMANCER = Path("Chronomancer", 'Master')
+CONJURER = Path("Conjurer", 'Master')
+CONQUEROR = Path("Conqueror", 'Master')
+DEATH_DEALER = Path("Death Dealer", 'Master')
+DEFENDER = Path("Defender", 'Master')
+DERVISH = Path("Dervish", 'Master')
+DESTROYER = Path("Destroyer", 'Master')
+DIPLOMAT = Path("Diplomat", 'Master')
+DIVINER = Path("Diviner", 'Master')
+DREADNAUGHT = Path("Dreadnaught", 'Master')
+DUELIST = Path("Duelist", 'Master')
+ENCHANTER = Path("Enchanter", 'Master')
+ENGINEER = Path("Engineer", 'Master')
+EXECUTIONER = Path("Executioner", 'Master')
+EXORCIST = Path("Exorcist", 'Master')
+EXPLORER = Path("Explorer", 'Master')
+GEOMANCER = Path("Geomancer", 'Master')
+GLADIATOR = Path("Gladiator", 'Master')
+GUNSLINGER = Path("Gunslinger", 'Master')
+HEALER = Path("Healer", 'Master')
+HEXER = Path("Hexer", 'Master')
+HYDROMANCER = Path("Hydromancer", 'Master')
+ILLUSIONIST = Path("Illusionist", 'Master')
+INFILTRATOR = Path("Infiltrator", 'Master')
+INQUISITOR = Path("Inquisitor", 'Master')
+JACK_OF_ALL_TRADES = Path("Jack of All Trades", 'Master')
+MAGE_KNIGHT = Path("Mage Knight", 'Master')
+MAGUS = Path("Magus", 'Master')
+MARAUDER = Path("Marauder", 'Master')
+MIRACLE_WORKER = Path("Miracle Worker", 'Master')
+MYRMIDON = Path("Myrmidon", 'Master')
+NECROMANCER = Path("Necromancer", 'Master')
+POISONER = Path("Poisoner", 'Master')
+PYROMANCER = Path("Pyromancer", 'Master')
+RUNESMITH = Path("Runesmith", 'Master')
+SAVANT = Path("Savant", 'Master')
+SENTINEL = Path("Sentinel", 'Master')
+SHAPESHIFTER = Path("Shapeshifter", 'Master')
+SHARPSHOOTER = Path("Sharpshooter", 'Master')
+STORMBRINGER = Path("Stormbringer", 'Master')
+TECHNOMANCER = Path("Technomancer", 'Master')
+TEMPLAR = Path("Templar", 'Master')
+TENEBRIST = Path("Tenebrist", 'Master')
+THAUMATURGE = Path("Thaumaturge", 'Master')
+THEURGE = Path("Theurge", 'Master')
+TRANSMUTER = Path("Transmuter", 'Master')
+TRAVELER = Path("Traveler", 'Master')
+WEAPON_MASTER = Path("Weapon Master", 'Master')
+WOODWOSE = Path("Woodwose", 'Master')
+ZEALOT = Path("Zealot", 'Master')
+MASTER_PATHS = [p for p in Path.items if p._type == 'Master']
 
 def choose_ancestory():
     return random.choice(ANCESTORIES)
@@ -335,10 +473,11 @@ def run_demo():
     i_item = choose_interesting_item()
     i_item.name = i_item.name.lower()
     if ancestory == ORC:
-        print(f'You are an {ancestory.name} {profession.name} with {i_item.name} as a keepsake')
+        print(f'You are an {ancestory.name} {profession.name} with {i_item.name[:-1]}')
     else:
-        print(f'You are a {ancestory.name} {profession.name} with {i_item.name} as a keepsake')
+        print(f'You are a {ancestory.name} {profession.name} with {i_item.name[:-1]}')
 
 
 for i in range(10):
-    run_demo()
+    # run_demo()
+    pass
