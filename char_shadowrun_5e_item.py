951558f3af4d1f27699263ec5256f576540e1ed2
refrshrs <nthorgan@live.co.uk>
2023-08-30 18:53:31 +0100

    item file stuff

--- char_shadowrun_5e_item.py
+++ char_shadowrun_5e_item.py
@@ -1,48 +1,60 @@
 import random
 import char_shadowrun_5e_data as Core
 
+DEFAULT_MAX_AVAILABILITY = 12
+DEFAULT_MAX_RATING = 6
+DEFAULT_AUG_GRADES = ["standard", "alphaware"]
+
+class Logger():
+    def __init__(self):
+        self.events = []
+
+LOG = Logger()
+
 def attrAsDict(_class):
     return {i: _class.__getattribute__(i) for i in dir(_class) if not i.startswith("__") and i != 'items'}
 
-def get_rating(item: Core.Gear):
+
+def get_rating(item: Core.Gear, max_rating=DEFAULT_MAX_RATING):
+    LOG.events.append(f"{item.name} rating to be calculated")
     if "rating" not in attrAsDict(item).keys():
         return 1
     if isinstance(item.rating, int):
+        LOG.events.append(f"{item.name} rating is int")
         return item.rating
     elif isinstance(item.rating, str):
         return 0
     elif isinstance(item.rating, list):
-        return random.choice(range(item.rating[0], item.rating[2]))
+        LOG.events.append(f"{item.name} rating is list, gone to list_handler")
+        return list_handler(item.rating, item, max_rating)
     else:
         raise ValueError(f'{item.name} has bad rating data')
 
+
 def get_item_cost(item: Core.Gear, arg=-1):
+    LOG.events.append(f"{item.name} cost to be calculated")
     if not hasattr(item, "cost"):
         raise AttributeError("Item has not 'cost' attribute")
 
     if isinstance(item.cost, int):
+        LOG.events.append(f"{item.name} cost is int")
         return item.cost
     elif isinstance(item.cost, list):
         if arg != -1:
             return list_handler(item.cost, item, arg)
-        return list_handler(item.cost, item)
+        return list_handler(item.cost, item, arg=True)
 
 
 def list_handler(l: list, item, arg=-1):
-    if item.name in [
-            'Gas Vent System',
-            'Chemical Protection',
-            'Fire Resistance',
-            'Insulation',
-            'Nonconductivity',
-            'Thermal Damping'
-        ]:
-        return 0
+
+    # if item.name in [ 'Gas Vent System', 'Chemical Protection', 'Fire Resistance', 'Insulation', 'Nonconductivity', 'Thermal Damping' ]:
+    #    return 0
     r1 = 1
 
     if isinstance(l[0], int) and l[1] == "to":
-        if arg != -1:
-            pass
+        if arg != -1 and isinstance(arg, int):
+            if arg < l[2]:
+                l[2] == arg
         else:
             return random.choice(range(l[0], l[2]))
         
@@ -109,19 +121,7 @@ def list_handler(l: list, item, arg=-1):
         case "*":
             if isinstance(r1, int) and isinstance(l[2], int):
                 return r1 * l[2]
-"""
-    match l[0]:
-        case "Rating":
-            if not hasattr(item, "rating"):
-                raise AttributeError(f"Item has no 'rating' attribute despite mention in {l}")
-            if arg != -1 and arg < item.rating[2] and arg > item.rating[0]:
-                l[0] = arg
-            else:
-                l[0] = get_rating(item)
-        case "Quantity":
-        case _:
-            print(f"TODO LIST HANDLER FOR {l[0]} in {l} for {item.name}")
-"""
+
 
 def get_mod(item: Core.Gear, m=None):
     if isinstance(item, Core.Firearm):
@@ -172,15 +172,38 @@ def build_sensor(arg=-1):
         else:
             x = random.choice([f"{k}: (Range: {d})" if d != 0 else f"{k}" for k, d in Core.SENSOR_FUNCTIONS.items()])
             sensor_functions.append(x)
+    sensor = Core.Sensor(sensor_type, sensor_housing, sensor_functions)
+    
+    return sensor
 
 
-    return Core.Sensor(sensor_type, sensor_housing, sensor_functions)
+def get_augmentation_grade(item: Core.Augmentation, grade=None, grades=DEFAULT_AUG_GRADES):
+    print(item.name)
+    grade_mods = {
+            'standard': {'cost': 1, 'avail': 0, 'essence': 1},
+            'alphaware': {'cost': 1.2, 'avail': 2, 'essence': 0.8},
+            'betaware': {'cost': 1.5, 'avail': 4, 'essence': 0.7},
+            'deltaware': {'cost': 2.5, 'avail': 8, 'essence': 0.5},
+            'used': {'cost': 0.75, 'avail': -4, 'essence': 1.25},
+            }
+
+    if grade is None:
+        grade = random.choice(grades)
+    item.grade = grade
+
+    print(get_item_cost(item))
+    item.cost = get_item_cost(item) * grade_mods[grade]['cost']
+    # item.avail = item.avail + grade_mods[grade]['avail']
+    # item.essence = item.essence * grade_mods[grade]['essence']
+
+    return item
+
+for aug in Core.Augmentation.items:
+    x = get_augmentation_grade(aug)
+    print(x.name, x.grade)
+    print("----", x.cost)
 
 
-
-#for item in Core.Gear.items:
-#    if hasattr(item, "requires"):
-#        print(item.name, item.requires)
 
 
 a = build_sensor()
